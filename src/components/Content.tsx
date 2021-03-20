a5198f415ff4242e563806fb06900f512dc27cf9
aureane.moraes <aureane.moraes@gmail.com>
2021-03-20 19:20:34 -0300

    desafio completado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
-export function Content() {
+interface ContentProps {
+  children: React.ReactNode;
+  selectedGenreTitle: string;
+}
+
+export function Content({children, selectedGenreTitle} : ContentProps) {
   // Complete aqui
+  // <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+  
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {children}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,17 @@
-export function SideBar() {
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  children: React.ReactNode;
+}
+export function SideBar({children} : SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {children}
+      </div>
+    </nav>
+  );
+
 }
